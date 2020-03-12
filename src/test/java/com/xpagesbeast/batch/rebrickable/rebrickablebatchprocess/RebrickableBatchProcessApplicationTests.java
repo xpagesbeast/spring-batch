334b6152066aa7907042a18d1898b783f9029aab
slobodan <slohja@hotmail.com>
2020-03-12 09:06:07 -0500

    first commit

---
+++ src/test/java/com/xpagesbeast/batch/rebrickable/rebrickablebatchprocess/RebrickableBatchProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpagesbeast.batch.rebrickable.rebrickablebatchprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebrickableBatchProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
